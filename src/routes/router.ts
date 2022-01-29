cdb603d7e5f7f0a29f2742c15f5a5ee3b7ec7058
pan93412 <pan93412@gmail.com>
2022-01-30 01:40:29 +0800

    chore(router): add log

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,9 @@
 import type { CheerioHandlePageInputs } from "apify";
+import { utils } from "apify";
 import type { BasicUserData, Handler, PageType } from "../types/router-types";
 import { UnhandledPageType } from "./exceptions/unhandled-page-type";
+
+const { log } = utils;
 
 export class Router {
     constructor(private readonly handlers: Record<PageType, Handler>) {}
@@ -13,6 +16,8 @@ export class Router {
     public async handle(inputs: CheerioHandlePageInputs): Promise<void> {
         const { type } = inputs.request.userData as BasicUserData;
         const handlers = this.handlers[type];
+
+        log.debug(`[Router] Handling: ${type}`);
 
         if (!handlers) throw new UnhandledPageType(type);
         await handlers.process(inputs);
