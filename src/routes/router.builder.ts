511764537258c8e994bd48a400f648889e140436
pan93412 <pan93412@gmail.com>
2022-01-31 19:56:19 +0800

    feat(router/builder): initiate handler in builder stage

--- src/routes/router.builder.ts
+++ src/routes/router.builder.ts
@@ -1,5 +1,12 @@
+import type TypedEventEmitter from "typed-emitter";
+import type { AvailableEvents } from "../event/emitter";
 import type { PageType, Handler } from "../types/router-types";
+import type { HandlerAbstract } from "./handler.abstract";
 import { Router } from "./router";
+
+export type HandlerConstructor = new (
+    emitter: TypedEventEmitter<AvailableEvents>
+) => HandlerAbstract;
 
 /**
  * Router 的建構類別。
@@ -7,8 +14,17 @@ import { Router } from "./router";
 export class RouterBuilder {
     private readonly handlerTable: Record<string, Handler> = {};
 
-    addRoute(type: PageType, handler: Handler): this {
-        this.handlerTable[type] = handler;
+    private emitter: TypedEventEmitter<AvailableEvents> | null = null;
+
+    setEmitter(emitter: TypedEventEmitter<AvailableEvents>): this {
+        this.emitter = emitter;
+        return this;
+    }
+
+    addRoute(type: PageType, Handler: HandlerConstructor): this {
+        if (!this.emitter) throw new Error("Emitter is not set.");
+
+        this.handlerTable[type] = new Handler(this.emitter);
         return this;
     }
 
