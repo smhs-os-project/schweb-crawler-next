4319d9e68255337511b914cb79e03b89824202da
pan93412 <pan93412@gmail.com>
2022-01-31 18:23:11 +0800

    feat(exporter): docs exporter

--- src/exporter/exporters/docs-exporter.ts
+++ src/exporter/exporters/docs-exporter.ts
@@ -1,8 +1,10 @@
+import { join } from "path";
 import type { Exporter } from "../../types/exporter-types";
+import { copyFile } from "../../utils/file";
 import { ExporterAbstract } from "../exporter.abstract";
 
 export class DocsExporter extends ExporterAbstract implements Exporter {
-    export(dataDir: string): Promise<void> {
-        throw new Error("Method not implemented.");
+    async export(dataDir: string): Promise<void> {
+        await copyFile(join(__dirname, "..", "data", "api-help.md"), dataDir);
     }
 }
